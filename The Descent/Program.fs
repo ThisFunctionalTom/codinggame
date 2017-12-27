de19d0285b8bf00d43fc61198f628697d41b9a15
Tomas Leko <leko.tomas@fronius.com>
2017-12-27 14:29:39 +0100

    The Descent

---
+++ The Descent/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec loop () =
+    List.init 8 (fun i -> i, Console.ReadLine() |> Int32.Parse)
+    |> List.maxBy snd
+    |> fst
+    |> printfn "%d"
+
+[<EntryPoint>]
+let main argv =
+    loop()
+    0 // return an integer exit code
